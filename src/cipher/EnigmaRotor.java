6ebc7e22fa33605a6c7d7f821c19cd13bbdaf2d6
Kevin Lawrence <kevin.lawrence@brentwood.bc.ca>
2013-03-13 10:55:39 -0700

    Enigma Update

--- src/cipher/EnigmaRotor.java
+++ src/cipher/EnigmaRotor.java
@@ -9,14 +9,39 @@ package cipher;
  * @author kevin.lawrence
  * reference: http://en.wikipedia.org/wiki/Enigma_rotor_details
  */
+
+//TODO: Refactor all this back into EnigmaRotorType, (and expose a factory there, or  "subset" functionality?)
 public enum EnigmaRotor {
-    I_C, II_C, III_C, 
-    I_R, II_R, III_R, UKW_R, ETW_R, 
-    I_K, II_K, III_K, UKW_K, ETW_K, 
-    I, II, III, IV, V, VI, VII, VIII, 
-    BETA, GAMMA, REF_A, REF_B, REF_C, REF_B_THIN, REF_C_THIN, REF_ETW;
+    I_C(EnigmaRotorType.COMMERCIAL), II_C(EnigmaRotorType.COMMERCIAL), III_C(EnigmaRotorType.COMMERCIAL), 
+    I_R(EnigmaRotorType.ROCKET), II_R(EnigmaRotorType.ROCKET), III_R(EnigmaRotorType.ROCKET), UKW_R(EnigmaRotorType.ROCKET), ETW_R(EnigmaRotorType.ROCKET), 
+    I_K(EnigmaRotorType.SWISS_K), II_K(EnigmaRotorType.SWISS_K), III_K(EnigmaRotorType.SWISS_K), UKW_K(EnigmaRotorType.SWISS_K), ETW_K(EnigmaRotorType.SWISS_K), 
+    I(EnigmaRotorType.ENIGMA_1), II(EnigmaRotorType.ENIGMA_1), III(EnigmaRotorType.ENIGMA_1), IV(EnigmaRotorType.M3_ARMY), V(EnigmaRotorType.M3_ARMY), VI(EnigmaRotorType.M3_M4_NAVAL), VII(EnigmaRotorType.M3_M4_NAVAL), VIII(EnigmaRotorType.M3_M4_NAVAL), 
+    BETA(EnigmaRotorType.M4_R2), GAMMA(EnigmaRotorType.M4_R2), REF_A(EnigmaRotorType.NONE), REF_B(EnigmaRotorType.NONE), REF_C(EnigmaRotorType.NONE), REF_B_THIN(EnigmaRotorType.M4_R1), REF_C_THIN(EnigmaRotorType.M4_R1), REF_ETW(EnigmaRotorType.ENIGMA_1);
  
-    private boolean reflector;
-    private String type;
     
+    //<editor-fold defaultstate="collapsed" desc="Constructors">
+    private EnigmaRotor(EnigmaRotorType rotorType){
+        this.rotorType = rotorType;
+  }
+        //</editor-fold>
+    
+    
+    //<editor-fold defaultstate="collapsed" desc="Properties">
+    private EnigmaRotorType rotorType;
+
+    /**
+     * @return the rotorType
+     */
+    public EnigmaRotorType getRotorType() {
+        return rotorType;
+    }
+
+    /**
+     * @param rotorType the rotorType to set
+     */
+    public void setRotorType(EnigmaRotorType rotorType) {
+        this.rotorType = rotorType;
+    }
+    //</editor-fold>
+ 
 }
---
+++ src/cipher/EnigmaRotorType.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cipher;
+
+/**
+ *
+ * @author kevin.lawrence
+ */
+public enum EnigmaRotorType {
+    COMMERCIAL("Commercial Enigma A, B"), ROCKET("German Railway [Rocket]"), SWISS_K("Swiss K"), ENIGMA_1("Enigma 1"), M3_ARMY("M3 Army"), M3_M4_NAVAL("M3 & M4 Naval"), M4_R2("M4 R2"), M4_R1("M4 R1"), NONE("");
+
+    
+    //<editor-fold defaultstate="collapsed" desc="Constructors">
+    private EnigmaRotorType(String model, boolean reflector) {
+        this.model = model;
+        this.reflector = reflector;
+    }
+    
+    private EnigmaRotorType(String model) {
+        this.model = model;
+        this.reflector = false;
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Properties">
+    private String model = "";
+    private boolean reflector;
+    
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+    
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * @return the reflector
+     */
+    public boolean isReflector() {
+        return reflector;
+    }
+
+    /**
+     * @param reflector the reflector to set
+     */
+    public void setReflector(boolean reflector) {
+        this.reflector = reflector;
+    }
+    //</editor-fold>
+    
+}
